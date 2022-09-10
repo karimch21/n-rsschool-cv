e7f3a19ea03b141d169316f3be2cc884b2ffec93
karimch21 <anonKM1999@mail.ru>
2022-09-10 23:05:56 +0300

    feat: added script for block selection on menu click and added style for selection

--- js/main.js
+++ js/main.js
@@ -7,6 +7,48 @@ burger.addEventListener('click', burgerClickHandler);
 window.addEventListener('load', () => {
     addBtnDetailsClickListener(btnProjectDetails)
 })
+window.addEventListener('click', windowClickHandler);
+
+function windowClickHandler(e) {
+    clickOutsideMenuHandler(e, menu, burger)
+    selectionSection(e)
+}
+
+function selectionSection(e) {
+    let menuItemLink = e.target.closest('.menu__item a')
+    if (!menuItemLink) return
+    let idSection = menuItemLink.href.slice(menuItemLink.href.indexOf('#'), );
+    let titleSection = document.querySelector(`${idSection} h2`);
+    if (!titleSection) return
+    let countSeletion = 0;
+    setTimeout(function amountSelection() {
+        titleSection.classList.toggle('title--select')
+        countSeletion += 400;
+        if (countSeletion >= 2400) {
+            titleSection.classList.remove('title--select')
+            return
+        }
+        setTimeout(amountSelection, countSeletion)
+    }, 1300)
+}
+
+function clickOutsideMenuHandler(e, menu, burger) {
+    let menuElement = e.target.closest('.menu');
+    let burgerElement = e.target.closest('.burger');
+    let menuItemElement = e.target.closest('.menu__item');
+
+    if (!burgerElement && !menuElement) {
+        removeActiveBurgerAndMenu(burger, menu)
+    } else if (menuItemElement) {
+        removeActiveBurgerAndMenu(burger, menu)
+    }
+}
+
+function removeActiveBurgerAndMenu(burger, menu) {
+    burger.classList.remove('burger--active')
+    menu.classList.remove('menu--active')
+}
+
 
 function addBtnDetailsClickListener(elements) {
     for (let el of elements) {
