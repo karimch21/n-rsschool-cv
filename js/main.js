13b8a489e75c562644f230e4f8842f25b1054e3d
karimch21 <anonKM1999@mail.ru>
2022-09-11 20:27:29 +0300

    feat: add script scrolling to section block, add grade

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', burgerClickHandler);
-
 window.addEventListener('load', () => {
     addBtnDetailsClickListener(btnProjectDetails)
 })
@@ -12,6 +11,17 @@ window.addEventListener('click', windowClickHandler);
 function windowClickHandler(e) {
     clickOutsideMenuHandler(e, menu, burger)
     selectionSection(e)
+    scrollTo(e);
+}
+
+function scrollTo(e) {
+    e.preventDefault()
+    let menuItemLink = e.target.closest('.menu__item a');
+    if (!menuItemLink) return;
+    let idSection = menuItemLink.href.slice(menuItemLink.href.indexOf('#'), );
+    let section = document.querySelector(`${idSection}`);
+    let distance = 20;
+    window.scrollBy({ top: [section.offsetTop - distance], left: 0, behavior: 'smooth' })
 }
 
 function selectionSection(e) {
@@ -24,7 +34,7 @@ function selectionSection(e) {
     setTimeout(function amountSelection() {
         titleSection.classList.toggle('title--select')
         countSeletion += 400;
-        if (countSeletion >= 2400) {
+        if (countSeletion >= 2000) {
             titleSection.classList.remove('title--select')
             return
         }
@@ -49,7 +59,6 @@ function removeActiveBurgerAndMenu(burger, menu) {
     menu.classList.remove('menu--active')
 }
 
-
 function addBtnDetailsClickListener(elements) {
     for (let el of elements) {
         el.addEventListener('click', projectDetailtClickHandler);
@@ -64,3 +73,12 @@ function burgerClickHandler() {
     this.classList.toggle('burger--active');
     menu.classList.toggle('menu--active')
 }
+
+console.log(`студент засабмитил в rs app ссылку на своё CV, задеплоенное на gh-pages +10
+вёрстка валидная +10\n 
+для проверки валидности вёрстки используйте сервис https://validator.w3.org/\n валидной вёрстке соответствует надпись "Document checking completed. No errors or warnings to show." В таком случае баллы за пункт требований выставляем полностью.\n 
+если есть предупреждения - warnings, но нет ошибок - errors, выставляем половину баллов за пункт требованийвёрстка семантическая +20\n 
+В коде странице присутствуют и используются по назначению следующие элементы: \n 
+Под "используются по назначению" имеется в виду, что у элементов есть какое-то содержание, соответствующее типу элемента, а не добавлены пустые теги для получения баллов. Правильность использования элементов не проверяется и на балл не влияет. \n header, main, footer +5\n элемент nav +5\n только один заголовок h1 +5\n заголовки h2 +5\n в footer есть ссылка на гитхаб автора работы, год создания приложения, логотип курса со ссылкой на курс +10\n Если ссылка в футере ведёт на курс stage0, это не ошибка. \n для оформления СV используются css-стили +10\n при уменьшении масштаба страницы браузера вёрстка размещается по центру по горизонтали, а не сдвигается в сторону +10\n
+на странице СV есть фото или аватарка автора, пропорции изображения не искажены, у изображения есть атрибут alt +10\n навигация, контакты для связи и перечень навыков оформлены в виде списка ul > li или ul > li > a +10\n Также допускается использование нумерованого списка ol > li или ol > li > a\n
+Содержание CV +30\n CV содержит:\n краткую информацию о себе +5\n контакты для связи +5\n перечень навыков +5\n пример кода. Код добавляется при помощи символов и тегов, а не картинкой +5\n перечень выполненных учебных проектов со ссылками на исходный код или страницу приложения +5\n информацию об образовании и уровне английского +5\n CV выполнено на английском языке +10`)
